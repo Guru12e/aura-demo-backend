be6defc9d6cec0e2c858ecdc44698d37ec755735
GURU VIJAY M <144215156+Guru12e@users.noreply.github.com>
2024-08-02 20:57:11 +0530

    Update prediction.route.js

--- routes/prediction.route.js
+++ routes/prediction.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   dailyPrediction,
   housePrediction,
+  lifePrediction,
 } from "../controller/prediction.controller.js";
 
 const router = express.Router();
 
 router.post("/daily", dailyPrediction);
 router.post("/house", housePrediction);
+router.post("/life", lifePrediction);
 
 export default router;
